1e7427adb926dc4acc4358123d8a15ea10da4d02
Jay Hepat <162289805+Jayhepat@users.noreply.github.com>
2025-08-26 22:57:48 +0530

    Add files via upload

---
+++ Structure-Union/main.c
@@ -0,0 +1,28 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 26, 2025
+ *      Author: jayhe
+ */
+#include<stdio.h>
+#include<stdint.h>
+
+//struct Address{
+union Address{
+	uint16_t shortAddr;
+	uint32_t longAddr;
+};
+int main(){
+	//struct Address addr;
+	union Address addr;
+	addr.shortAddr = 0XABCD;
+	addr.longAddr = 0XFFFFAAAA;
+
+	printf("Short address: %x \n",addr.shortAddr);
+	fflush(stdout);
+	printf("Long address:%x\n",addr.longAddr);
+	fflush(stdout);
+	printf("Size of Address: %u\n ", sizeof(addr));
+	//fflush(stdout);
+	return 0;
+}
